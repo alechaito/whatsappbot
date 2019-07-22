0f25fc9f516922f09622ad3f78857d5bb735cc3a
alechaito <alechaito@gmail.com>
2019-07-22 18:19:23 -0400

    fix functions send, open, getname

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
@@ -9,6 +8,7 @@ import time
 import os
 from PIL import Image
 from io import BytesIO
+import logging
 
 class Whatsapp:
 
@@ -17,6 +17,7 @@ class Whatsapp:
 		self.url = "http://web.whatsapp.com"
 		self.driver.get(self.url)
 
+	## Initializing the session or reuse a existing
 	def create_driver_session(self):
 		arch = open('drive.txt', 'r')
 		session_id = arch.readline().strip()
@@ -43,44 +44,65 @@ class Whatsapp:
 		RemoteWebDriver.execute = org_command_execute
 		return new_driver
 
-	def get_new_msgs(self):
-		unread = self.get_elem_class("_1ZMSM") # The green dot tells us that the message is new
-		#unread = self.driver.find_elements_by_class_name("OUeyt")
-		#element = self.get_elem_class("X7YrQ")
-		name, message  = '', ''
-		print(len(unread))
-		print(unread[-1])
-		# click submit button
-		submit_button = self.driver.find_elements_by_xpath('//*[@class="_1ZMSM"]')[-1]
-		submit_button.click()
-
+	## Open a specify box chat
+	def open_box(self, box):
+		box.click()
+		time.sleep(0.3)
+		box.click()
+		print('[+] Box oppened with success...')
+	
+	## Need to be executed after open box to sucess
+	def get_contact_name(self):
 		try:
-			action.click()
-			action.perform()
-			time.sleep(3)
-			action.click()
-			action.perform()
-			print("nem vi")
+			return self.get_xpath('.//span[@class = "_19RFN"]')[-1].text
 		except:
-			pass
-		#name = self.get_elem_class("_1wjpf")[0].text  # Contact name
-		#msgs = self.drive.get_elem_class("vW7d1") # the message content
-		#last_msg = msgs[-1]
-		#prev_msg = msgs[-2]
-		#print(name)
+			return None
 
+	def send_message(self, msg):
+		text_box = self.get_elem_class("_13mgZ")[-1]
+		text_box.send_keys(msg)
+		submit = self.get_elem_class("hnQHL")[-1]
+		submit.click()
+		#text_box.send_keys(Keys.ENTER)
+		print("[+] Message send with sucess.")
+
+	## Return all unread messages
+	def get_unread_msgs(self):
+		return self.get_xpath('//*[@class="_2UaNq _2ko65"]')
+	
+	def get_qrcode(self):
+		qr_code = self.get_elem_class("_1pw2F _2VkjG")
+		if(len(qr_code) > 0):
+			try:
+				os.remove("./data/screenshot.png")
+				print("[+] Removed a QR Code SS.")
+			except:
+				pass
+			self.driver.save_screenshot("./data/screenshot.png")
+			print("[+] Saved a new screen shot from QR Code.")
+
+	# Shortcuts to selenium
 	def get_elem_class(self, class_name):
 		return self.driver.find_elements_by_class_name(class_name)
+	
+	def get_xpath(self, xpath):
+		return self.driver.find_elements_by_xpath(xpath)
+	
+	############################################
 
 def main():
 	Whats = Whatsapp()
 	## Get the QR Code from browser
-	qr_code = Whats.get_elem_class("_1pw2F")
-	#img = Whats.driver.find_element_by_tag_name('img')
 	time.sleep(5)
-	#img = Whats.driver.save_screenshot("./data/screenshot.png")
-	Whats.get_new_msgs()
-	#im = Image.open(BytesIO(img))
+	Whats.get_qrcode()
+	msgs = Whats.get_unread_msgs()
+	print(len(msgs))
+	for msg in msgs:
+		Whats.open_box(msg)
+		#Whats.get_contact_name()
+		Whats.send_message("test1")
+		Whats.send_message("test2")
+		Whats.send_message("test3")
 
 if __name__ == "__main__":
     main()
